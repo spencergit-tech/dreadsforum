f47b439a6f341fdd4d646ef0b74e09ed157a56e6
spencergit-tech <spencerkvikstad@gmail.com>
2025-02-13 10:33:00 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 // Set up database connection using environment variables
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL || "postgresql://postgres:nuxtxfgeaeoosizrqqkv@db.nuxtxfgeaeoosizrqqkv.supabase.co:5432/postgres"
-  ssl: {rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
 });
 
 // Test database connection
